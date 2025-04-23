3729d6ada0d4358d4c4d109d5c7ed76071c7336d
Clb184 <84605945+Clb184@users.noreply.github.com>
2025-04-22 19:51:20 -0500

    Añadir conexión a base de datos, TODO resolver un problema de refresh en login

--- JS/login.js
+++ JS/login.js
@@ -4,7 +4,7 @@ const registerForm = document.getElementById("register-form");
 const formTitle = document.getElementById("form-title");
 
 toggleLink.addEventListener("click", (e) => {
-  e.preventDefault();
+  //e.preventDefault();
 
   // Quiero evitar hacer un montón de branching por usar el short-hand if else (a ? b : c)
   // así que separé todo con una sola evaluación, no debería cambiar nada - Sebastián
@@ -20,4 +20,31 @@ toggleLink.addEventListener("click", (e) => {
     formTitle.textContent ="Iniciar Sesión";
     toggleLink.textContent = "¿No tienes cuenta? Crear una";
   }
+});
+$( document ).ready(function() {
+
+        $( "button" ).click(function( event ) {
+        $.ajax({
+              url: "http://localhost:8080/login", // Replace with your actual endpoint URL
+              type: "GET",
+              data: { name: "adm", password: "12345" }, // Optional data to send
+              success: function(response) {
+                // Handle the response from the Java method
+                console.log("Response from Java:", response);// sleep time expects milliseconds
+                function sleep (time) {
+                  return new Promise((resolve) => setTimeout(resolve, time));
+                }
+                
+                // Usage!
+                sleep(1000).then(() => {
+                    // Do something after the sleep!
+                });
+                // Update UI or perform other actions
+              },
+              error: function(xhr, status, error) {
+                  // Handle errors
+                  console.error("Error calling Java method:", status, error);
+              }
+        });
+    });
 });
